70c04215ab6996f53f78629e0384eb2819714933
deguzmanarieljames <arieljamesdeguzman321@gmail.com>
2023-10-12 23:03:53 +0800

    add_ons commit

--- src/main.js
+++ src/main.js
@@ -4,9 +4,27 @@ import router from './router'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 
+
+
+
+import './assets/js/jquery.1.8.3.min';
+import './assets/js/wow.min';
+import './assets/js/featherlight.min';
+import './assets/js/featherlight.gallery.min';
+import './assets/js/jquery.enllax.min';
+import './assets/js/jquery.scrollUp.min';
+import './assets/js/jquery.easing.min';
+import './assets/js/jquery.stickyNavbar.min';
+import './assets/js/jquery.waypoints.min';
+import './assets/js/images-loaded.min';
+import './assets/js/lightbox.min';
+import './assets/js/site';
+
 loadFonts()
 
 createApp(App)
   .use(router)
   .use(vuetify)
   .mount('#app')
+
+
---
+++ src/views/ManageView.vue
@@ -0,0 +1,20 @@
+<template>
+  <Manage />
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+// Components
+import Manage from '../components/Manage.vue';
+
+export default defineComponent({
+  name: 'ManageView',
+
+  components: {
+    Manage,
+  },
+});
+
+
+</script>
